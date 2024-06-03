ba218d943430e941664d32986d3526d4422b710c
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-06-03 14:28:54 -0300

    feat: Project structure

---
+++ dbconfig.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+
+export const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+}
---
+++ src/services/pizzas-services.js
@@ -0,0 +1,28 @@
+import {config} from "../../dbconfig.js"
+import {createConnection} from "mysql2/promise"
+
+const connection  = createConnection(config)
+
+export async function getAll() {
+    const [rows, _] = await connection.query('SELECT * FROM Pizza')
+    return rows
+}
+
+export async function getById(id) {
+    const [rows, _] = await connection.query('SELECT * FROM Pizza WHERE id = ?', [id])
+    if (rows.length === 0) {
+        return null
+    } else {
+        return rows[0]
+    }
+}
+
+export async function insert(pizza) {
+    const [result, _] = await connection.query('INSERT INTO Pizza (nombre, libreGluten, importe, descripcion) VALUES (?, ?, ?, ?)', [pizza.nombre, pizza.libreGluten, pizza.importe, pizza.descripcion])
+    return result
+}
+
+export async function update(pizza) {
+    const [result, _] = await connection.query('UPDATE Pizza SET nombre = ?, libreGluten = ?, importe = ?, descripcion = ? WHERE id = ?', [pizza.nombre, pizza.libreGluten, pizza.importe, pizza.descripcion, pizza.id])
+    return result
+}
