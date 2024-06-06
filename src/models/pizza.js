f7f9006a46937e1bd9ecd202fc386c3b1aaa9c15
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-06-06 11:34:37 -0300

    feat(Pizza structure): Added constructor to pizza class

--- src/models/pizza.js
+++ src/models/pizza.js
@@ -1,4 +1,12 @@
 class Pizza {
+
+    constructor(nombre, libreGluten, importe, descripcion) {
+        this.nombre = nombre;
+        this.libreGluten = libreGluten;
+        this.importe = importe;
+        this.descripcion = descripcion;
+    }
+
     id;
     nombre;
     libreGluten;
