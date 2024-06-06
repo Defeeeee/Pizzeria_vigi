d907b2678c9591c40020a0187e493415b2e0b6a4
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-06-06 11:35:08 -0300

    feat: Added express REST API config

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import * as db from "./src/services/pizzas-services.js"
+import Pizza from "./src/models/pizza.js";
+
+import express from 'express';
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.get('/pizzas', async (req, res) => {
+    const pizzas = await db.getAll();
+    res.json(pizzas);
+});
+
+app.get('/pizzas/:id', async (req, res) => {
+    const id = req.params.id;
+    const pizza = await db.getById(id);
+    if (pizza) {
+        res.json(pizza);
+    } else {
+        res.status(404).send();
+    }
+});
+
+var server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
